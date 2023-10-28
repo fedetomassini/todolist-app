b9ea1e261d2b28fc26c9ca9d8c332bada779ef8f
Federico Tomassini <fedetomassini2@gmail.com>
2023-10-28 19:31:34 -0300

    [Initial Commit]

---
+++ src/components/core/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Wrapper from './Wrapper';
+
+const App = () => {
+    return (
+        <>
+        <Wrapper>
+            
+        </Wrapper>
+        </>
+  )
+}
+
+export default App;
